eedfa4e429e7bc1dc0fb40ef1bf5180a1f9abc4b
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-07-02 11:08:56 +0200

    Complete bar chart

--- bar-chart/app.js
+++ bar-chart/app.js
@@ -37,5 +37,119 @@ d3.json(
       .attr("y", height + 50)
       .text("More Information: http://www.bea.gov/national/pdf/nipaguid.pdf")
       .attr("class", "info");
+
+    const years = data.data.map(function (item) {
+      let quarter;
+      const temp = item[0].substring(5, 7);
+
+      if (temp === "01") {
+        quarter = "Q1";
+      } else if (temp === "04") {
+        quarter = "Q2";
+      } else if (temp === "07") {
+        quarter = "Q3";
+      } else if (temp === "10") {
+        quarter = "Q4";
+      }
+
+      return item[0].substring(0, 4) + " " + quarter;
+    });
+
+    const yearsDate = data.data.map(function (item) {
+      return new Date(item[0]);
+    });
+
+    const xMax = new Date(d3.max(yearsDate));
+    xMax.setMonth(xMax.getMonth() + 3);
+    const xScale = d3
+      .scaleTime()
+      .domain([d3.min(yearsDate), xMax])
+      .range([0, width]);
+
+    const xAxis = d3.axisBottom().scale(xScale);
+
+    const xAxisGroup = svgContainer
+      .append("g")
+      .call(xAxis)
+      .attr("id", "x-axis")
+      .attr("transform", "translate(60, 400)");
+
+    const GDP = data.data.map(function (item) {
+      return item[1];
+    });
+
+    let scaledGDP = [];
+
+    const gdpMin = d3.min(GDP);
+    const gdpMax = d3.max(GDP);
+
+    const linearScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
+
+    scaledGDP = GDP.map(function (item) {
+      return linearScale(item);
+    });
+
+    const yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([height, 0]);
+
+    const yAxis = d3.axisLeft(yAxisScale);
+
+    const yAxisGroup = svgContainer
+      .append("g")
+      .call(yAxis)
+      .attr("id", "y-axis")
+      .attr("transform", "translate(60, 0)");
+
+    d3.select("svg")
+      .selectAll("rect")
+      .data(scaledGDP)
+      .enter()
+      .append("rect")
+      .attr("data-date", function (d, i) {
+        return data.data[i][0];
+      })
+      .attr("data-gdp", function (d, i) {
+        return data.data[i][1];
+      })
+      .attr("class", "bar")
+      .attr("x", function (d, i) {
+        return xScale(yearsDate[i]);
+      })
+      .attr("y", function (d, i) {
+        return height - d;
+      })
+      .attr("width", barWidth)
+      .attr("height", function (d) {
+        return d;
+      })
+      .style("fill", "#33adff")
+      .attr("transform", "translate(60, 0)")
+      .on("mouseover", function (d, i) {
+        overlay
+          .transition()
+          .duration(0)
+          .style("height", d + "px")
+          .style("width", barWidth + "px")
+          .style("opacity", 0.9)
+          .style("left", i * barWidth + 0 + "px")
+          .style("top", height - d + "px")
+          .style("transform", "translateX(60px)");
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip
+          .html(
+            years[i] +
+              "<br>" +
+              "$" +
+              GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
+              " Billion"
+          )
+          .attr("data-date", data.data[i][0])
+          .style("left", i * barWidth + 30 + "px")
+          .style("top", height - 100 + "px")
+          .style("transform", "translateX(60px)");
+      })
+      .on("mouseout", function (d) {
+        tooltip.transition().duration(200).style("opacity", 0);
+        overlay.transition().duration(200).style("opacity", 0);
+      });
   }
 );
