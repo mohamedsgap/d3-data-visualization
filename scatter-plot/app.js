66a664c75b7886b21aa80c7ec48086b3fbc8bd72
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-07-08 04:45:25 +0200

    Define coordinates

--- scatter-plot/app.js
+++ scatter-plot/app.js
@@ -11,5 +11,17 @@ const margin = {
   bottom: 30,
   left: 60,
 };
+
 const width = 920 - margin.left - margin.right;
 const height = 630 - margin.top - margin.bottom;
+
+const x = d3.scaleLinear().range([0, width]);
+const y = d3.scaletime().range([0, height]);
+
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+const timeFormat = d3.timeFormat("%M:%S");
+
+const xAxis = d3.axisBottom(x).tickFormat(d3.format("d"));
+
+const yAxis = d3.axisLeft(y).tickFormat(timeFormat);
